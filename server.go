dd1f033e23bd33c8d9e5143e76dee2da7ba0792b
CCPupp <cjerskine@gmail.com>
2021-05-12 20:19:51 -0400

    Update

--- server.go
+++ server.go
@@ -9,7 +9,6 @@ import (
 	"strings"
 
 	_ "github.com/bmizerany/pq"
-	"github.com/kabukky/httpscerts"
 )
 
 func main() {
@@ -19,33 +18,24 @@ func main() {
 	var headerTemplate = "headerTemplate.html"
 	var navTemplate = "navTemplate.html"
 
-	// Check if the cert files are available.
-	err := httpscerts.Check("cert.pem", "key.pem")
-	// If they are not available, generate new ones.
-	if err != nil {
-		err = httpscerts.Generate("cert.pem", "key.pem", "127.0.0.1:8080")
-		if err != nil {
-			log.Fatal("Error: Couldn't create https certs.")
-		}
-	}
 	// Handler points to available directories
 	http.Handle("/home/", http.StripPrefix("/home/", http.FileServer(http.Dir("home"))))
 	http.Handle("/scripts/", http.StripPrefix("/scripts/", http.FileServer(http.Dir("scripts"))))
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, readFile(headerTemplate))
+		fmt.Fprint(w, readFile(headerTemplate))
 		if strings.Contains(r.URL.Path[1:], "projects") {
-			fmt.Fprintf(w, readFile(navProjectsTemplate))
+			fmt.Fprint(w, readFile(navProjectsTemplate))
 		} else {
-			fmt.Fprintf(w, readFile(navTemplate))
+			fmt.Fprint(w, readFile(navTemplate))
 		}
 		if r.URL.Path[1:] == "" {
 			http.ServeFile(w, r, "home/index.html")
 		} else {
 			http.ServeFile(w, r, "home/"+r.URL.Path[1:]+".html")
 		}
-		fmt.Fprintf(w, readFile(footerTemplate))
+		fmt.Fprint(w, readFile(footerTemplate))
 	})
 
 	//Serves local webpage for testing
@@ -69,19 +59,4 @@ func readFile(fileName string) string {
 		return "Error parsing file."
 	}
 	return string(data)
-}
-
-func checkErr(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
-func find(slice []string, val string) (int, bool) {
-	for i, item := range slice {
-		if item == val {
-			return i, true
-		}
-	}
-	return -1, false
 }
